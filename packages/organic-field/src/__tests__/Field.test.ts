9767bf481161669bc31d4ba0830caf292aa59baf
Hilary <h.t.osborne@gmail.com>
2020-09-12 23:58:26 -0700

    feat(*): added ability for field to be set and getted

--- packages/organic-field/src/__tests__/Field.test.ts
+++ packages/organic-field/src/__tests__/Field.test.ts
@@ -3,6 +3,29 @@ import Field from '../Field';
 
 describe('Organic/Field', () => {
   it('can create a simple organic field', () => {
-    expect(true).to.equal(true);
+    const exampleField = Field<void>('exampleField', 'STRING');
+    expect(exampleField.machine).to.equal('exampleField');
+    expect(exampleField.context).to.equal('STRING');
+    expect(exampleField.value()).to.be.undefined;
+  });
+
+  it('can provide a simple method for getting and setting the value', () => {
+    const exampleField = Field<void | string>('exampleField', 'STRING');
+    exampleField.value('John');
+    expect(exampleField.value()).to.equal('John');
+  });
+
+  it('can allow the set value to be overwritten', () => {
+    const exampleField = Field<void | string>('exampleField', 'STRING');
+    exampleField.value('John');
+    exampleField.value('Barry');
+    expect(exampleField.value()).to.equal('Barry');
+  });
+
+  it('can allow for the value to be set to undefined', () => {
+    const exampleField = Field<void | string>('exampleField', 'STRING');
+    exampleField.value('John');
+    exampleField.value(undefined);
+    expect(exampleField.value()).to.be.undefined;
   });
 });
