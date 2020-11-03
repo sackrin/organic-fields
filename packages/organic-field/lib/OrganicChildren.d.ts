18c8096a87f7f12eaf0a87501f4a01a2acc7b1e3
Hilary <h.t.osborne@gmail.com>
2020-11-03 00:21:25 -0800

    feat(*): removed lib

--- packages/organic-field/lib/OrganicChildren.d.ts
+++
@@ -1,6 +0,0 @@
-declare class OrganicChildren<V> extends Array {
-    constructor();
-    value(): V;
-}
-export default OrganicChildren;
-//# sourceMappingURL=OrganicChildren.d.ts.map
--- packages/organic-field/lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { default as OrganicChildren } from './OrganicChildren';
-export { default as OrganicCollection } from './OrganicCollection';
-export { default as OrganicContainer } from './OrganicContainer';
-export { default as OrganicProperty } from './OrganicProperty';
-//# sourceMappingURL=index.d.ts.map
